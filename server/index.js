fa4203298dc2b0256d72df485ea47b9e79b21153
yuvicodes <yuvrajsingh17831@gmail.com>
2024-09-01 04:07:45 +0530

    user db model

--- server/index.js
+++ server/index.js
@@ -3,13 +3,29 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectToDatabase from "./config/db.js";
 connectToDatabase();
+import usermodel from "./models/user.model.js";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get("/",(req,res)=>{
-    res.send("heheh");
+    res.send("Hello World");
+})
+
+app.post("/api/user",async (req,res)=>{
+    const {username,password,email,state,city,postalArea} = req.body;
+    const user = await usermodel.create({
+        username,
+        password,
+        email : email,
+        state,
+        city,
+        postalArea
+    });
+    res.send(user);
 })
 
 app.listen(PORT,()=>{
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,2 +1,33 @@
 import mongoose from "mongoose";
 
+const user = mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    
+    state : {
+        type : String,
+        required : true
+    },
+    city : {
+        type : String,
+        required : true
+    },
+    postalArea : {
+        type : String,
+        required : true
+    }
+})
+
+export default mongoose.model("User", user);
